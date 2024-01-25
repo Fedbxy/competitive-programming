d35c7b296a2742052141dbc57fe26df6897d9638
Fed <punnaphop.sitthi@gmail.com>
2024-01-25 21:29:17 +0700

    add full sol for 1672

--- CSES/1672.cpp
+++ CSES/1672.cpp
@@ -2,51 +2,31 @@
 
 using namespace std;
 using ll = long long;
-using pii = pair<int,int>;
-using pli = pair<ll,int>;
-
-const int N = 510;
-vector<pii> adj[N];
-vector<vector<ll>> dist(N, vector<ll>(N, LLONG_MAX));
-
-void dijkstra(int s, int idx) {
-    vector<bool> flag(N);
-    priority_queue<pli, vector<pli>, greater<pli>> pq;
-    dist[s][idx] = 0;
-    pq.push(make_pair(dist[s][idx], s));
-
-    while(!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
-
-        if(flag[u]) continue;
-        flag[u] = true;
-
-        for(auto vw:adj[u]) {
-            int v = vw.first, w = vw.second;
-            if(flag[v]) continue;
-            if(dist[v][idx] > dist[u][idx] + w) {
-                dist[v][idx] = dist[u][idx] + w;
-                pq.push(make_pair(dist[v][idx], v));
-            }
-        }
-    }
-}
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     int n,m,q;cin>>n>>m>>q;
-    for(int i=0;i<m;i++) {
-        int u,v,w;cin>>u>>v>>w;
-        adj[u].push_back(make_pair(v,w));
-        adj[v].push_back(make_pair(u,w));
+    ll dist[n+1][n+1];
+    for(int i=1;i<=n;i++) for(int j=1;j<=n;j++) {
+        if(i == j) dist[i][j] = 0;
+        else dist[i][j] = LLONG_MAX;
     }
-
-    for(int i=1;i<=n;i++) dijkstra(i, i);
-
+    for(int i=0;i<m;i++) {
+        int u,v;ll w;cin>>u>>v>>w;
+        dist[u][v] = min(dist[u][v], w);
+        dist[v][u] = min(dist[v][u], w);
+    }
+    for(int k=1;k<=n;k++) {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(dist[i][k] == LLONG_MAX or dist[k][j] == LLONG_MAX) continue;
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
     while(q--) {
         int u,v;cin>>u>>v;
-        cout<<(dist[v][u] == LLONG_MAX ? -1 : dist[v][u])<<'\n';
+        cout<<(dist[u][v] == LLONG_MAX ? -1 : dist[u][v])<<'\n';
     }
     return 0;
 }
