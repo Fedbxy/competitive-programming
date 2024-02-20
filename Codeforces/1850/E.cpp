1c5ff315110d73c66690ec41ba4388863ad44296
Fed <punnaphop.sitthi@gmail.com>
2024-02-20 23:00:22 +0700

    add 1850E

--- Codeforces/1850/E.cpp
+++ Codeforces/1850/E.cpp
@@ -3,16 +3,37 @@
 using namespace std;
 using ll = long long;
 
+bool ok(vector<int> &a, ll c, ll x) {
+    ll sum = 0;
+    for(auto e:a) {
+        sum += (2 * x + e) * (2 * x + e);
+        if(sum > c) break;
+    }
+    // cout << " = " << sum;
+    return sum >= c;
+}
+
 void solve() {
-    ll n,c;cin>>n>>c;
-    vector<ll> a(n);
-    for(int i=0;i<n;i++) cin>>a[i];
-    
+    int n; cin >> n;
+    ll c; cin >> c;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin >> a[i];
+    ll l = 1, r = 1e9;
+    while(l < r) {
+        ll mid = l + (r - l) / 2;
+        // cout << "#" << mid;
+        if(ok(a, c, mid)) {
+            // cout << " true";
+            r = mid;
+        } else l = mid + 1;
+        // cout << '\n';
+    }
+    cout << r << '\n';
 }
 
 int main() {
-    ios::sync_with_stdio(false);cin.tie(0);
-    int t=1;cin>>t;
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t; cin >> t;
     while(t--) solve();
     return 0;
 }
