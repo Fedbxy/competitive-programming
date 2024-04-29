38e70d6a0bf88c4556fff69b1be66d9805cea0af
Fed <punnaphop.sitthi@gmail.com>
2024-04-30 01:51:23 +0700

    add runner template

---
+++ LeetCode/runner.template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include "sol.cpp"
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+
+    Solution sol;
+    cout << sol.function() << '\n'; 
+    return 0;
+}
